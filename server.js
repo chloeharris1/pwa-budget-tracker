a8c7ebd96fb75ba1d6208417918044335918f0e4
Chloe Harris <chloe.a.harris17@gmail.com>
2021-11-16 18:51:37 -0800

    updated server file

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/budget", {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
 
 // routes
 app.use(require("./routes/api.js"));
